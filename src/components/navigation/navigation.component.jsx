5f6d374cd053a627d30755543052f09a2d24daf4
J.D. Browne <PeshwariNaan01@proton.me>
2022-05-16 16:38:59 +0400

    Add routing and Navigation component

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to={'/'}>
+          <div>LOGO</div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to={"/shop"}>
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
