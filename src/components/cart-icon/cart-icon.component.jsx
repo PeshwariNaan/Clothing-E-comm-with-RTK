20084d51346a2b9172563b17f48ea745d886f047
J.D. Browne <PeshwariNaan01@proton.me>
2022-05-20 17:05:34 +0400

    Added the cart item component for the dropdown and all functionallity in CartContext to keep track of quantity

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
 
     const toggleisCartOpen = () => {
         setIsCartOpen(!isCartOpen)
@@ -13,7 +13,7 @@ const CartIcon = () => {
   return (
     <div className='cart-icon-container' onClick={toggleisCartOpen}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>10</span>
+        <span className='item-count'>{cartCount}</span>
     </div>
   )
 }
