c8d76ded4276308c4de834e18c76ecca731e2150
J.D. Browne <PeshwariNaan01@proton.me>
2022-05-23 16:26:42 +0400

    Modified all routes for styled components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,10 +1,11 @@
-import { useContext, useState, useEffect, Fragment } from "react";
-import { useParams } from "react-router-dom";
-import ProductCard from "../../components/product-card/product-card.component";
+import { useContext, useState, useEffect, Fragment } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { CategoriesContext } from "../../contexts/categories.context";
-import "./category.styles.scss";
+import ProductCard from '../../components/product-card/product-card.component';
 
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
@@ -15,19 +16,15 @@ const Category = () => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
     <Fragment>
-      <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className='category-container'>
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
+      </CategoryContainer>
     </Fragment>
   );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import CategoriesPreview from "../categories-preview/categories-preview.component";
-import Category from "../category/category.component";
-import "./shop.styles.scss";
+import { Routes, Route } from 'react-router-dom';
 
- 
-  
-  const Shop = () => {
-    return (
-      <Routes>
-        <Route index element={<CategoriesPreview />} />
-        <Route path=':category' element={<Category />} />
-      </Routes>
-    );
-  };
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
-export default Shop
+const Shop = () => {
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
+};
+
+export default Shop;
