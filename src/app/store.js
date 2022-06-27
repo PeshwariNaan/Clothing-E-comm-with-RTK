295f0f7a4760ac2020a8b6990d1b72ee703d4f52
J.D. Browne <PeshwariNaan01@proton.me>
2022-06-27 12:52:13 +0400

    Added logic for userSlice and added the reducer to the new store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
-
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import userReducer from '../features/user/userSlice';
 
 const reducers = {
-
-}
+    user: userReducer,
+};
 
 export const store = configureStore({
-    reducers,
-    middleware: [logger]
-})
+  reducers,
+  middleware: [logger],
+});
